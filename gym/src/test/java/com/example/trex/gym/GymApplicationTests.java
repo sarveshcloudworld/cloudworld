5864eac3a136558d12024ba0958026f429d16a05
Sarvesh Patil <patilsarvesh804@gmail.com>
2026-09-24 15:18:57 +0530

    Add gym project

---
+++ gym/src/test/java/com/example/trex/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trex.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
